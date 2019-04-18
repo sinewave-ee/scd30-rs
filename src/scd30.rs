4dd5b9942704aa378fbe7391cd4064b8b31d35d0
Vincent Stakenburg <vincentstakenburg@gmail.com>
2019-04-18 16:45:52 +0200

    add documentation, add more safety to functions

--- src/scd30.rs
+++ src/scd30.rs
@@ -16,58 +16,98 @@ pub enum Command {
     SoftReset                   = 0xd304,
 }
 
+const EXPECT_MSG: &str = "Vec was not large enough";
 const ADDRESS: u8 = 0x61 << 1;
 
 pub struct Scd30<T> {
-    i2c: T,
+    comm:    T,
+    address: u8,
 }
 
 pub struct Measurement {
-    pub co2:            f32,
-    pub humidity:       f32,
-    pub temperature:    f32,
+    pub co2:         f32,
+    pub humidity:    f32,
+    pub temperature: f32,
 }
 
+/// See the [datasheet] for I²c parameters.
+///
+/// [datasheet]: https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/9.5_CO2/Sensirion_CO2_Sensors_SCD30_Interface_Description.pdf
 impl<T, E> Scd30<T> where T: Read<Error = E> + Write<Error = E> {
-
-    pub fn new(mut i2c: T) -> Self {
+    /// Returns an [Scd30] instance with the default address 0x61 shifted one place to the left.
+    /// You may or may not need this bitshift depending on the byte size of
+    /// your [I²c](embedded_hal::blocking::i2c) peripheral.
+    pub fn new(i2c: T) -> Self {
         Scd30 {
-            i2c
+            comm: i2c,
+            address: ADDRESS
         }
     }
 
-    pub fn soft_reset(&mut self) -> Result<(), <T as Write>::Error> {
-        self.i2c.write(ADDRESS, &(Command::SoftReset as u16).to_be_bytes())
+    /// Returns an [Scd30] instance with the specified address.
+    pub fn new_with_address(i2c: T, address: u8) -> Self {
+        Scd30 {
+            comm: i2c,
+            address
+        }
     }
 
-    pub fn start_measuring(&mut self) {
+    pub fn soft_reset(&mut self) -> Result<(), E> {
+        self.comm.write(self.address, &(Command::SoftReset as u16).to_be_bytes())
+    }
+
+    pub fn stop_measuring(&mut self) -> Result<(), E> {
+        self.comm.write(self.address, &(Command::StopContinuousMeasurement as u16).to_be_bytes())
+    }
+
+    /// Enable or disable automatic self calibration (ASC).
+    ///
+    /// According to the datasheet, the sensor should be active continously for at least
+    /// 7 days to find the initial parameter for ASC. The sensor has to be exposed to
+    /// at least 1 hour of fresh air (~400ppm CO₂) per day.
+    pub fn set_automatic_calibration(&mut self, enable: bool) -> Result<(), E> {
+        let mut vec: Vec<u8, U4> = Vec::new();
+        vec.extend_from_slice(&(Command::SetAutomaticSelfCalibration as u16).to_be_bytes()).expect(EXPECT_MSG);
+        vec.extend_from_slice(&[ 0x00, enable as u8 ]).expect(EXPECT_MSG);
+        self.comm.write(self.address, &vec)
+    }
+
+    /// Start measuring without mbar compensation.
+    pub fn start_measuring(&mut self) -> Result<(), E> {
         self.start_measuring_with_mbar(0)
     }
 
-    pub fn set_automatic_calibration(&mut self, enable: bool) {
+    /// Start measuring with mbar (pressure) compensation.
+    pub fn start_measuring_with_mbar(&mut self, pressure: u16) -> Result<(), E> {
         let mut vec: Vec<u8, U4> = Vec::new();
-        vec.extend_from_slice(&(Command::SetAutomaticSelfCalibration as u16).to_be_bytes());
-        vec.extend_from_slice(&[ 0x00, enable as u8 ]);
-        self.i2c.write(ADDRESS, &vec);
+        vec.extend_from_slice(&(Command::StartContinuousMeasurement as u16).to_be_bytes()).expect(EXPECT_MSG);
+        vec.extend_from_slice(&pressure.to_be_bytes()).expect(EXPECT_MSG);
+        self.comm.write(self.address, &vec)
     }
 
-    pub fn start_measuring_with_mbar(&mut self, pressure: u16) {
-        let mut vec: Vec<u8, U4> = Vec::new();
-        vec.extend_from_slice(&(Command::StartContinuousMeasurement as u16).to_be_bytes());
-        vec.extend_from_slice(&pressure.to_be_bytes());
-        self.i2c.write(ADDRESS, &vec);
+    pub fn data_ready(&mut self) -> Result<bool, E> {
+        let mut buf = [0u8; 2];
+        self.comm.write(self.address, &(Command::GetDataReadyStatus as u16).to_be_bytes())?;
+        self.comm.read(self.address, &mut buf)?;
+        Ok(u16::from_be_bytes(buf) == 1)
     }
 
-    pub fn read(&mut self) -> Measurement {
-        let mut buf = [0u8; 6 * 3];
+    pub fn read(&mut self) -> Result<Option<Measurement>, E> {
+        match self.data_ready() {
+            Ok(true) => {
+                let mut buf = [0u8; 6 * 3];
 
-        self.i2c.write(ADDRESS, &(Command::ReadMeasurement as u16).to_be_bytes());
-        self.i2c.read(ADDRESS, &mut buf);
+                self.comm.write(self.address, &(Command::ReadMeasurement as u16).to_be_bytes())?;
+                self.comm.read(self.address, &mut buf)?;
 
-        Measurement {
-            co2:         f32::from_bits(u32::from_be_bytes([ buf[0], buf[1], buf[3], buf[4] ])),
-            temperature: f32::from_bits(u32::from_be_bytes([ buf[6], buf[7], buf[9], buf[10] ])),
-            humidity:    f32::from_bits(u32::from_be_bytes([ buf[12], buf[13], buf[15], buf[16] ])),
+                Ok(Some(Measurement {
+                    co2:         f32::from_bits(u32::from_be_bytes([ buf[0],  buf[1],  buf[3],  buf[4] ])),
+                    temperature: f32::from_bits(u32::from_be_bytes([ buf[6],  buf[7],  buf[9],  buf[10] ])),
+                    humidity:    f32::from_bits(u32::from_be_bytes([ buf[12], buf[13], buf[15], buf[16] ])),
+                }))
+            },
+            Ok(false) => Ok(None),
+            Err(e) => Err(e),
         }
     }
 
